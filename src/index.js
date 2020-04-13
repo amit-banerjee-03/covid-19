cf16291056f7ce5890a6ac40fa7a39823cd69cb3
Amit Banerjee <parkouramit@gmail.com>
2020-04-13 14:06:57 +0530

    Navbar added

--- src/index.js
+++ src/index.js
@@ -1,105 +1,28 @@
-import React, { Component, PureComponent } from 'react';
+import React, { Component } from 'react';
 import ReactDOM, { render } from 'react-dom';
+import Home from './pages/home';
+import NavBar from './includes/header';
 import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-} from 'recharts';
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
-export default class Example extends PureComponent {
-  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/xqjtetw0/';
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      Countries: []
-    };
-  }
 
-  componentDidMount() {
-    fetch("https://api.covid19api.com/summary")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            Countries: result.Countries
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      );
-    var scriptsToLoad = [
-      "https://unpkg.com/react/umd/react.production.min.js",
-      "https://unpkg.com/react-dom/umd/react-dom.production.min.js",
-      "https://unpkg.com/prop-types/prop-types.min.js",
-      "https://unpkg.com/recharts/umd/Recharts.min.js"
-    ];
 
-    for (var index in scriptsToLoad) {
-      var script = document.createElement("script");
-      script.src = scriptsToLoad[index];
-      script.async = true;
-      document.body.appendChild(script);
-    }
-  }
+export default class Covid19 extends Component {
 
   render() {
-    const { error, isLoaded, Countries } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      var data = [];
-      Countries.map(Country => (
-        data.push({ name: Country.Country, "Total Confirmed": Country.TotalConfirmed, "Total Recovered": Country.TotalRecovered, "Total Deaths": Country.TotalDeaths })
-      ));
-      return (
-        <center>
-          <div style={{
-            fontFamily: "sans-serif",
-            color: "#ba0000"
-          }}>
-            <h1>Worldwide COVID-19 cases</h1>
-          </div>
-          <div style={{
-            width: "60%",
-            overflow: "scroll",
-            padding: "50px"
-          }}>
-            <LineChart
-              width={12000}
-              height={500}
-              data={data}
-              margin={{
-                top: 5, right: 30, left: 20, bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line type="monotone" dataKey="Total Confirmed" stroke="#8884d8" activeDot={{ r: 8 }} />
-              <Line type="monotone" dataKey="Total Recovered" stroke="#82ca9d" />
-              <Line type="monotone" dataKey="Total Deaths" stroke="#ff0000" />
-            </LineChart>
-          </div>
-        </center>
-      );
-    }
+    return (
+      <Router>
+        <NavBar />
+        <Route path="/" component={Home} exact></Route>
+      </Router>
+    );
   }
 }
 
 render(
-  <Example />,
+  <Covid19 />,
   document.getElementById('root')
 );
 
---
+++ src/pages/home.js
@@ -0,0 +1,100 @@
+import React, { Component, PureComponent } from 'react';
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+
+class Home extends PureComponent {
+    static jsfiddleUrl = 'https://jsfiddle.net/alidingling/xqjtetw0/';
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            Countries: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://api.covid19api.com/summary")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        Countries: result.Countries
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            );
+        var scriptsToLoad = [
+            "https://unpkg.com/react/umd/react.production.min.js",
+            "https://unpkg.com/react-dom/umd/react-dom.production.min.js",
+            "https://unpkg.com/prop-types/prop-types.min.js",
+            "https://unpkg.com/recharts/umd/Recharts.min.js"
+        ];
+
+        for (var index in scriptsToLoad) {
+            var script = document.createElement("script");
+            script.src = scriptsToLoad[index];
+            script.async = true;
+            document.body.appendChild(script);
+        }
+    }
+
+    render() {
+        const { error, isLoaded, Countries } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            var data = [];
+            Countries.map(Country => (
+                data.push({ name: Country.Country, "Total Confirmed": Country.TotalConfirmed, "Total Recovered": Country.TotalRecovered, "Total Deaths": Country.TotalDeaths })
+            ));
+            return (
+                <center>
+                    <div style={{
+                        fontFamily: "sans-serif",
+                        color: "#ba0000"
+                    }}>
+                        <h1>Worldwide COVID-19 cases</h1>
+                    </div>
+                    <div style={{
+                        width: "60%",
+                        overflow: "scroll",
+                        padding: "50px"
+                    }}>
+                        <LineChart
+                            width={12000}
+                            height={500}
+                            data={data}
+                            margin={{
+                                top: 5, right: 30, left: 20, bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line type="monotone" dataKey="Total Confirmed" stroke="#8884d8" activeDot={{ r: 8 }} />
+                            <Line type="monotone" dataKey="Total Recovered" stroke="#82ca9d" />
+                            <Line type="monotone" dataKey="Total Deaths" stroke="#ff0000" />
+                        </LineChart>
+                    </div>
+                </center>
+            );
+        }
+    }
+}
+
+export default Home;
