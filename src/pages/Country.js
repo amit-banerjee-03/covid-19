227fdb5c57cd722807bf641b49cd4ea82c0a0f86
Aneesha Das Windows <das.aneesha.ad@gmail.com>
2020-04-14 13:52:36 +0530

    working

--- src/pages/Country.js
+++ src/pages/Country.js
@@ -5,6 +5,20 @@ import {
 
   export default function CountryStats(){
     let { id } = useParams();
+    let isLoaded=false;
+    var countries=[];
+    let errorOccured=null;
+    fetch('https://api.covid19api.com/countries')
+        .then(res => res.json())
+        .then((result) => {
+          countries=result;
+          isLoaded=true;
+        },
+          (error) => {
+            isLoaded=true;
+            errorOccured=error;
+          })
+      console.log(countries);
   return (
     <div>
       <h3>ID: {id}</h3>
