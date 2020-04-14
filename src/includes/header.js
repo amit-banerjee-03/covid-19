911467f2070931e01007e9f1fd1613b8efdf7d0a
Amit Banerjee <parkouramit@gmail.com>
2020-04-14 12:44:40 +0530

    minor ui changes

--- src/includes/header.js
+++ src/includes/header.js
@@ -5,7 +5,7 @@ import coronaImage from '../images/corona-image.png'
 const NavBar = () => {
     return (
         <>
-            <Navbar bg="dark" variant="dark">
+            <Navbar fixed="top" bg="dark" variant="dark">
                 <Navbar.Brand href="/"><img src={coronaImage} width="50px" height="50px"/> Covid-19 Monitor</Navbar.Brand>
                 <Nav className="mr-auto">
                     {/* <Nav.Link href="/">Home</Nav.Link> */}
