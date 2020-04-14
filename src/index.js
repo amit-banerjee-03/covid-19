350b87749c5fb028ee0a499ea3779faad0a99809
Aneesha Das Windows <das.aneesha.ad@gmail.com>
2020-04-14 11:55:05 +0530

    working on select

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM, { render } from 'react-dom';
 import Home from './pages/home';
-import CountryStats from './pages/Country-Stats';
+import CountryStats, {Child} from './pages/CountryStats';
 import NotFound from './pages/not-found';
 import NavBar from './includes/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import {
   BrowserRouter as Router,
   Route,
@@ -23,6 +22,7 @@ export default class Covid19 extends Component {
         <Switch>
           <Route path="/" component={Home} exact></Route>
           <Route path="/country" component={CountryStats} exact></Route>
+          <Route path="/country/:id" children={Child} exact></Route>
           <Route component={NotFound}></Route>
         </Switch>
       </Router>
