17fe9ed9692545a8aff9add9ed1e69b43c637ea9
Amit Banerjee <parkouramit@gmail.com>
2020-04-18 15:00:03 +0530

    Ui changes

--- src/pages/countries.js
+++ src/pages/countries.js
@@ -61,7 +61,7 @@ export default class CountryDropdown extends Component {
         <center>
           <div style={{ width: "98%",overflow:"hide" }}>
             <div className="row">
-              <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12" style={{ backgroundColor: "#343a40" }}>
+              <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12" style={{ backgroundColor: "#343a40",height:"88vh" }}>
                 <div className="mt-5">
                   <SelectSearch
                     key="countries"
@@ -88,7 +88,7 @@ export default class CountryDropdown extends Component {
                 </div>
                 <CountryListStats countries={this.state.countries} />
               </div>
-              <div className="col-lg-9 col-md-9 col-sm-12 col-xs-12" style={{ color: "#d21c1cc4", fontWeight: "bold", fontSize: "35px", maxHeight: "90vh", overflowY: "scroll" }}>{selectCountry}</div>
+              <div className="col-lg-9 col-md-9 col-sm-12 col-xs-12" style={{ color: "#d21c1cc4", fontWeight: "bold", fontSize: "35px", maxHeight: "88vh", overflowY: "scroll" }}>{selectCountry}</div>
             </div>
           </div>
         </center>
